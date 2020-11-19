b3fdf2e10372b99c27b272cb9d88e58be45b4ba3
diogocsouza <diogocsouza@users.noreply.github.com>
2020-11-19 17:08:22 -0300

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,40 @@ const sequelize = new Sequelize('test', 'root', 'admin', {
 }).catch(function (erro){
     console.log("Deu ruim "+erro)
 })*/
+
+//criacao de models para criar tabelas do banco
+
+const Postagem = sequelize.define('postagens', {
+    titulo:{
+        type: Sequelize.STRING
+    },
+    conteudo:{
+        type:Sequelize.TEXT
+    }
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome:{
+        type: Sequelize.STRING
+    },
+    sobrenome:{
+        type:Sequelize.STRING
+    },
+    idade:{
+        type:Sequelize.INTEGER
+    },
+    email:{
+        type:Sequelize.STRING
+    }
+})
+
+//metodo para criar tabela, cuidade para nao executar 2x pode duplicar
+//Usuario.sync({force: true})
+
+//metodo para fazer insert into **nodemon em execução duplica entradas
+Postagem.create({
+    titulo: "Bla",
+    conteudo:"Bleaaaaaaaa"
+})
+
+
