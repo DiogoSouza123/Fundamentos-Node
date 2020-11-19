97096e945492fd49b60757c80dbce6c82af1c08d
diogocsouza <diogocsouza@users.noreply.github.com>
2020-11-19 14:45:09 -0300

    .

--- test.js
+++ test.js
@@ -4,8 +4,10 @@ const sequelize = new Sequelize('test', 'root', 'admin', {
     dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function (){
+//funcao para falar se conectou ou não ao banco de dados
+
+/*sequelize.authenticate().then(function (){
     console.log("Conectado com sucesso")
 }).catch(function (erro){
     console.log("Deu ruim "+erro)
-})
+})*/
